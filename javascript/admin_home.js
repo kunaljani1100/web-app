0e34af040dae2be00157575a7a89034c6b369cf9
unknown <kunaljani18@gmail.com>
2021-03-04 23:12:49 -0800

    Change password

--- javascript/admin_home.js
+++ javascript/admin_home.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
         e.preventDefault();
         window.location.href = "add_project.html";
     })
+    $("#change-password").on("click", (e) => {
+        e.preventDefault();
+        window.location.href = "change_password.html";
+    })
     $("#add-person").on("click", (e) => {
         e.preventDefault();
         window.location.href = "add_person.html";
